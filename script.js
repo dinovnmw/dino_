a68615326e4a3ddfbf467cf35307ef071b4f9934
dinovnmw <monstersteam290423@gmail.com>
2025-01-22 16:15:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+// Chạy hiệu ứng animation khi trang được tải
+document.addEventListener('DOMContentLoaded', () => {
+  const profileImg = document.querySelector('.profile-img');
+  profileImg.classList.add('animate');
+});
 
